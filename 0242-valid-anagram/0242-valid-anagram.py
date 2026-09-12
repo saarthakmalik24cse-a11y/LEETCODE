f2309a765dfd807fc9ac667f85ddf75905fd6f0f
saarthakmalik24cse-a11y <saarthak.malik.24cse@bmu.edu.in>
2026-09-12 13:28:39 +0530

    Time: 11 ms (77.48%), Space: 19.5 MB (46.07%) - LeetHub - Time: O(n), Space: O(n)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,15 @@
 class Solution:
     def isAnagram(self, s, t):
 
-        # If lengths are different,
-        # they cannot be anagrams
+    
         if len(s) != len(t):
             return False
 
-        # Create two empty hashmaps
+       
         count_s = {}
         count_t = {}
 
-        # Count characters of s
+       
         for char in s:
 
             if char in count_s:
@@ -18,7 +17,7 @@ class Solution:
             else:
                 count_s[char] = 1
 
-        # Count characters of t
+        
         for char in t:
 
             if char in count_t:
@@ -26,7 +25,7 @@ class Solution:
             else:
                 count_t[char] = 1
 
-        # Compare both hashmaps
+       
         return count_s == count_t
 
 # Synced seamlessly with LeetHub Pro
