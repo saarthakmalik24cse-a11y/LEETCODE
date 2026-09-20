b86e4e6f0a4aa422fd82580a3fb76953ed0173dd
saarthakmalik24cse-a11y <saarthak.malik.24cse@bmu.edu.in>
2026-09-20 23:53:46 +0530

    Time: 2 ms (91.77%), Space: 20.4 MB (48.03%) - LeetHub - Time: O(n), Space: O(1)

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,19 +1,19 @@
-class Solution:
-    def pivotIndex(self, nums):
+class Solution :
+    def pivotIndex(self , nums):
 
         total = sum(nums)
-        left_sum = 0
+        left = 0
 
         for i in range(len(nums)):
+            right = total - left - nums[i]
 
-            right_sum = total - left_sum - nums[i]
-
-            if left_sum == right_sum:
+            if left == right:
                 return i
 
-            left_sum = left_sum + nums[i]
+            left += nums[i]     
 
         return -1
+    
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
