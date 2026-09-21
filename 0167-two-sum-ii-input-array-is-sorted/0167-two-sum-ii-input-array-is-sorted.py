e85207e6c10a8baa24ca26686225e7e2a01e8be9
saarthakmalik24cse-a11y <saarthak.malik.24cse@bmu.edu.in>
2026-09-21 12:15:39 +0530

    Time: 9 ms (8.47%), Space: 22.3 MB (10.36%) - LeetHub - Time: O(n), Space: O(1)

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,21 +1,22 @@
-class Solution:
-    def twoSum(self, numbers, target):
+class Solution :
+    def twoSum(self,numbers,target):
 
         left = 0
-        right = len(numbers) - 1
+        right = len(numbers)-1
 
-        while left < right:
+        while left < right :
+            total = numbers[left] + numbers[right]
 
-            current_sum = numbers[left] + numbers[right]
+            if total == target:
+                return[left + 1, right + 1]
 
-            if current_sum == target:
-                return [left + 1, right + 1]
-
-            elif current_sum < target:
+            elif total < target:
                 left += 1
-
             else:
-                right -= 1
+                right -=1
+
+
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
