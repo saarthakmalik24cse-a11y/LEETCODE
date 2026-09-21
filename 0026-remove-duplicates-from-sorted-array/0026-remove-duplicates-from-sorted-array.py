3e30d1ca6e585199775e9f30e9c5e8b8aa29029c
saarthakmalik24cse-a11y <saarthak.malik.24cse@bmu.edu.in>
2026-09-21 12:41:31 +0530

    Time: 0 ms (100%), Space: 20.6 MB (43.63%) - LeetHub - Time: O(n), Space: O(1)

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,15 +1,16 @@
-class Solution:
-    def removeDuplicates(self, nums):
+class Solution :
+    def removeDuplicates(self,nums):
 
-        slow = 1
+        if not nums:
+            return 0
 
-        for fast in range(1, len(nums)):
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[j]!=nums[i]:
 
-            if nums[fast] != nums[fast - 1]:
-                nums[slow] = nums[fast]
-                slow += 1
-
-        return slow
+                i +=1
+                nums[i]=nums[j]
+        return i + 1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
