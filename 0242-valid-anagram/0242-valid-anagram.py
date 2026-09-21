4fb54742e5d6f86994932c4b28a47aa4e704619f
saarthakmalik24cse-a11y <saarthak.malik.24cse@bmu.edu.in>
2026-09-21 17:49:35 +0530

    Time: 15 ms (43.02%), Space: 19.2 MB (99.4%) - LeetHub - Time: O(n), Space: O(n)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,29 +1,23 @@
 class Solution :
-    def isAnagram(nums,s,t):
+    def isAnagram(self,s,t):
 
         if len(s) != len(t):
             return False 
 
         count = {}
 
-        for char in s:
-            count[char ] = count.get(char,0)+1
-
+        for char in s :
+            count[char] = count.get(char,0)+1
         for char in t :
 
             if char not in count :
-                return False
+                return False 
             
-            count[char] -= 1
+            count[char] -=1
 
             if count[char]<0:
                 return False 
-
         return True
-            
-
-
-            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
