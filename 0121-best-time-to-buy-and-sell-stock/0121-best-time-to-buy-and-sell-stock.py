cdf770f39ca4a5330fcb099720d307050615a76f
saarthakmalik24cse-a11y <saarthak.malik.24cse@bmu.edu.in>
2026-09-22 01:43:37 +0530

    Time: 50 ms (53.29%), Space: 28.6 MB (42.94%) - LeetHub - Time: O(n), Space: O(1)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,14 @@
 class Solution:
     def maxProfit(self, prices):
-
-        minimum_price = prices[0]
+        min_price = prices[0]
         max_profit = 0
 
         for price in prices:
+            min_price = min(min_price, price)
 
-            if price < minimum_price:
-                minimum_price = price
+            profit = price - min_price
 
-            profit = price - minimum_price
-
-            if profit > max_profit:
-                max_profit = profit
+            max_profit = max(max_profit, profit)
 
         return max_profit
 
